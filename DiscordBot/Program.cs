0efacf7aedcb991c2447d1807c9100da67eb2a4a
omg.itsjan <42674570+omgitsjan@users.noreply.github.com>
2022-12-09 22:27:02 +0100

    init

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,74 @@
+﻿using Discord;
+using Discord.WebSocket;
+using OpenAI_Discord_Bot.Service;
+
+namespace OpenAI_Discord_Bot;
+
+public class Program
+{
+    /// <summary>
+    ///     This is the Discord token from the bot - (Replace this with your Discord bot token)
+    /// </summary>
+    private const string DiscordToken = "";
+
+    /// <summary>
+    ///     Init
+    /// </summary>
+    private static void Main()
+    {
+        MainAsync().GetAwaiter().GetResult();
+    }
+
+    /// <summary>
+    ///     Main Program
+    /// </summary>
+    /// <returns></returns>
+    public static async Task MainAsync()
+    {
+        //Creates a config with specified gateway intents
+        var config = new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        };
+
+        // Create a new Discord client
+        var client = new DiscordSocketClient(config);
+
+        // Log messages to the console
+        client.Log += Log;
+
+        // Handle messages received
+        client.MessageReceived += HandleCommand;
+
+        // Login to Discord
+        await client.LoginAsync(TokenType.Bot, DiscordToken);
+
+        // Start the client
+        await client.StartAsync();
+
+        // Block this program until it is closed
+        await Task.Delay(-1);
+    }
+
+    /// <summary>
+    ///     This method is called whenever a message is received
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    private static async Task HandleCommand(SocketMessage message)
+    {
+        // Check if the message starts with the !chat command
+        if (message.Content.StartsWith("!chat")) await OpenAiService.ChatGpt(message);
+    }
+
+    /// <summary>
+    ///     This method logs messages to the console
+    /// </summary>
+    /// <param name="msg"></param>
+    /// <returns></returns>
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
---
+++ DiscordBot/Service/OpenAiService.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using RestSharp;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace OpenAI_Discord_Bot.Service;
+
+public class OpenAiService
+{
+    /// <summary>
+    ///     Api Key to access ChatGPT - (Replace this with your ChatGPT API key)
+    /// </summary>
+    private const string ChatGptApiKey = "";
+
+    /// <summary>
+    ///     Url to the ChatGpt Api
+    /// </summary>
+    private const string ChatGptApiUrl = "https://api.openai.com/v1/completions";
+
+    internal static async Task<bool> ChatGpt(SocketMessage message)
+    {
+        // Create a new RestClient instance
+        var client = new RestClient(ChatGptApiUrl);
+
+        // Create a new RestRequest instance
+        var request = new RestRequest("", Method.Post);
+
+        // Set the request headers
+        request.AddHeader("Content-Type", "application/json");
+        request.AddHeader("Authorization", $"Bearer {ChatGptApiKey}");
+
+        // Create the request data
+        var data = new
+        {
+            // The prompt is everything after the !chat command
+            model = "text-davinci-003",
+            prompt = message.Content[4..],
+            max_tokens = 256
+        };
+
+        var jsonData = JsonSerializer.Serialize(data);
+
+        // Add the request data to the request body
+        request.AddJsonBody(jsonData);
+
+        // Send the request and get the response
+        var response = await client.ExecuteAsync(request);
+
+        // Holds the response from the API.
+        string? responseText;
+        var success = false;
+        // Check the status code of the response
+        if (response.Content != null && response.StatusCode == HttpStatusCode.OK)
+        {
+            // Get the response text from the API
+            responseText = JsonConvert.DeserializeObject<dynamic>(response.Content)?["choices"][0]["text"];
+            success = true;
+        }
+        else
+        {
+            // Get the ErrorMessage from the API
+            responseText = response.ErrorMessage;
+        }
+
+        // Send the response to the Discord chat
+        await message.Channel.SendMessageAsync(responseText);
+        return success;
+    }
+}
