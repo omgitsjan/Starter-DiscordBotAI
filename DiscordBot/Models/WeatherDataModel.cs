95dd32f64a7ea2fc291c7eb4c8a90df7701e4724
Jan <42674570+omgitsjan@users.noreply.github.com>
2023-03-28 01:15:34 +0200

    Added Weather with OpenWeatherMap Api also added the Status but its not fully tested

---
+++ DiscordBot/Models/WeatherDataModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscordBot.Models;
+
+public class WeatherData
+{
+    public string? City { get; set; }
+    public string? Description { get; set; }
+    public string? WeatherIcon { get; set; }
+    public double? FeelsLike { get; set; }
+    public int? Humidity { get; set; }
+    public double? WindSpeed { get; set; }
+    public double? Temperature { get; set; }
+}
