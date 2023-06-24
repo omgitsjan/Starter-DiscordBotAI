4d52f5dfc97d5775a402a76b74ddf47d5108a976
Jan <42674570+omgitsjan@users.noreply.github.com>
2023-06-24 15:11:12 +0200

    Update to docker-compose

--- build.ps1
+++
@@ -1,9 +0,0 @@
-# Build script
-param(
-    [string]$dockerfilePath = "Dockerfile",
-    [string]$imageName = "discordbotai",
-    [string]$tag = "latest"
-)
-
-$command = 'docker build -f {0} -t {1}:{2} .' -f $dockerfilePath, $imageName, $tag
-Invoke-Expression $command
